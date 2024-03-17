f9fbef7c79f59a790124bc8cbf95c8913a311443
cyberme0w <me@cyberme0w.dev>
2024-03-17 19:53:38 +0100

    Fetch token from DISCORDO_TOKEN env var

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/ayn2op/discordo/cmd"
 	"github.com/ayn2op/discordo/internal/constants"
@@ -10,21 +11,29 @@ import (
 )
 
 func main() {
-	// Declare and parse all flags first
-	token := flag.String("token", "", "The authentication token.")
+	// Fetch config from environment variables
+	tokenEnv := os.Getenv("DISCORDO_TOKEN")
+
+	// Fetch config from CLI flags
+	tokenFlag := flag.String("token", "", "The authentication token.")
 	flag.Parse()
 
+	// Merge configs
+	var token string
+	if tokenEnv != "" { token = tokenEnv }
+	if *tokenFlag != "" { token = *tokenFlag }
+
 	// If no token was provided, look it up in the keyring
-	if *token == "" {
+	if token == "" {
 		t, err := keyring.Get(constants.Name, "token")
 		if err != nil {
 			log.Println("Authentication token not found in keyring:", err)
 		} else {
-			*token = t
+			token = t
 		}
 	}
 
-	if err := cmd.Run(*token); err != nil {
+	if err := cmd.Run(token); err != nil {
 		log.Fatal(err)
 	}
 }
